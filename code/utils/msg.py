af75e1a8e519fb5907f6ef21bbff3a83b48cd519
Swimitha <swimitha@gmail.com>
2022-10-04 19:15:07 -0400

    Added Offense Count check

--- code/utils/msg.py
+++ code/utils/msg.py
@@ -1,4 +1,5 @@
 import re
+from .db import DbConnector
 
 
 def cleanMessage(msg):
@@ -24,7 +25,23 @@ def getMsgTemplate(user_id, reason="profane", warning=False):
     reason_dict = {"profane": "using profane words"}
     ret = ""
     if warning:
-        ret = """ Warning user:<@{0}> for {1}.\n Another attempt will result in a Ban!""".format(user_id, reason_dict[reason])
+        ret = """ Warning user:<@{0}> for {1}.\n Another attempt will result in a Ban!""".format(user_id,
+                                                                                                 reason_dict[reason])
     else:
         ret = """ User:<@{0}> has been banned for {1}.""".format(user_id, reason_dict[reason])
     return ret
+
+
+def checkOffenseCount(user_id, reason):
+    dbconnect = DbConnector()
+    sql_query = "SELECT offense_count FROM user_activity where user_id = %s"
+    dbconnect.connect()
+    result = dbconnect.executequery(sql_query, (user_id,))
+    if result == 1:
+        getMsgTemplate(user_id, reason, True)
+    else:
+        ##Ban User
+        sql_query = "DELETE FROM user where user_id = %s"
+        result = dbconnect.executequery(sql_query, (user_id,))
+        getMsgTemplate(user_id, reason, False)
+    return True
