52ea3193ecc732081274a89a4815217265614ace
vamsitadikonda <vamsitadikonda99@gmail.com>
2022-10-04 19:55:11 -0400

    Updating checkers module info

---
+++ code/checkers/__init__.py
@@ -0,0 +1,7 @@
+"""
+Checkers Module:
+This module contains all the checks that can be done after parsing a message.
+The module contains multiple classes each targeting an individual function.
+The classes are organized based on the broad area they are responsible for.
+The main functions are Bully, Apology and Profanity with scope to add more.
+"""
