1f3e9b4b014251fb453f5f925fa71b75b7559ade
bkataba <bkataba@ncsu.edu>
2022-10-05 04:06:58 -0400

    Adding bully logic in on_message workflow

--- src/run.py
+++ src/run.py
@@ -1,5 +1,6 @@
 import sys
-from pathlib import Path # if you haven't already done so
+from pathlib import Path  # if you haven't already done so
+
 file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
@@ -7,8 +8,9 @@ sys.path.append(str(root))
 # Additionally remove the current file's directory from sys.path
 try:
     sys.path.remove(str(parent))
-except ValueError: # Already removed
+except ValueError:  # Already removed
     pass
+
 import os
 import discord
 from dotenv import load_dotenv
@@ -16,6 +18,7 @@ import src.checkers.profanity
 from src.checkers.apology import ApologyChecker
 from src.checkers.reporter import ReportChecker
 from src.utils.msg import clean_message, get_msg_template, get_help_message
+from src.checkers.bully import BullyChecker
 
 load_dotenv("bot.env")
 token = os.getenv("token")
@@ -52,21 +55,30 @@ async def on_message(message):
         await message.delete()
         # Step2.1 : Checking if the user has a first time offense
         warning = ac.check_user(aid, "profane")
-        await message.channel.send(get_msg_template(aid, "profane", warning))
+        await message.channel.send(get_msg_template(aid, "profanity", warning))
         # Step2.2: Banning user if not a first-time offense
         # ToDo: Writing Ban logic
     # Step 3: Check for Bully & Toxic Traits
-    # ToDo: Write Bully check
+    traits = bc.check_message(msg_content)
+    if len(traits) > 0:
+        await message.delete()
+        # Step3.1 : Checking if the user has a first time offense
+        warning = ac.check_user(aid, traits[0])
+        await message.channel.send(get_msg_template(aid, traits, warning))
+        # Step3.2: Banning user if not a first-time offense
+        # ToDo: Writing Ban logic
+
     # Step 4: Check for Apology
     if ac.check_message(msg_content):
         ac.add_apology(aid)
     # Step 5: Reporting a Profane Word
     if rc.check_message(msg_content):
-        report_type,report_token = rc.parse_message()
+        report_type, report_token = rc.parse_message()
         if report_type == "word":
             rc.report_word()
 
 
+bc = BullyChecker()
 ac = ApologyChecker()
 pc = src.checkers.profanity.ProfanityChecker()
 rc = ReportChecker()
