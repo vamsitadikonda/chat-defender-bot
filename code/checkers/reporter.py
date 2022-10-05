eec1f99df5482cce53e2cb739865f9046cf470a2
vamsitadikonda <vamsitadikonda99@gmail.com>
2022-10-05 00:24:58 -0400

    Adding reporter class

---
+++ code/checkers/reporter.py
@@ -0,0 +1,12 @@
+from code.checkers import Checker
+
+
+class ReportChecker(Checker):
+    def check_message(self, msg:str):
+        return msg.startswith('!report')
+
+    def parse_message(self,msg):
+        return msg.split(" ")[1], msg.split(" ")[2]
+
+    def report_word(self, word):
+        pass
