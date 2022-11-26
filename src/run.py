c37d4fda4ae7f78f1cc9fa70a63706e1a34c0e9b
Srujan Ponnur <srujanponnur@gmail.com>
2022-11-25 23:44:25 -0500

    changed flow of message checking

--- src/run.py
+++ src/run.py
@@ -44,13 +44,21 @@ async def on_message(message):
     if author == client.user:
         return
     # Step 1: Pre-process message
+    print(f"The message is: {message.content}, The channel is: {channel_name}, The Author Info: {author}-{author_id}")
     msg_content = clean_message(str(message.content))
     if msg_content.find('hello') != -1:
         await message.reply("Hey <@{0}> how's it going?".format(author_id))
     if msg_content.find('help') != -1:
         await message.reply(get_help_message())
-    # Step 2: Check for profanity
-    if pc.check_message(channel_name, msg_content):
+    
+     # Step 2: Reporting a Profane Word
+    if rc.check_message(msg_content):
+        report_type, report_token = rc.parse_message(msg_content)
+        if report_type == "word":
+            if pc.add_words(channel_name, report_token):
+                await message.reply("{0} has been added as a toxic word".format(report_token))
+    # Step 3: Check for profanity
+    elif pc.check_message(channel_name, msg_content):
         # Step2.1 : Checking if the user has a first time offense
         warning = ac.check_user_for_warning(author_id, channel_name)
         await message.channel.send(get_msg_template(author_id, "profanity", warning))
@@ -59,8 +67,9 @@ async def on_message(message):
         if not warning:
             await message.author.ban()
 
-    # Step 3: Check for Bully & Toxic Traits
+    # Step 4: Check for Bully & Toxic Traits
     traits = bc.check_message(msg_content)
+    print("The traits are", traits)
     if len(traits) > 0:
         # Step3.1 : Checking if the user has a first time offense
         warning = ac.check_user_for_warning(author_id, channel_name)
@@ -70,17 +79,12 @@ async def on_message(message):
         if not warning:
             await message.author.ban()
 
-    # Step 4: Check for Apology
-    if ac.check_message(msg_content):
+    # Step 5: Check for Apology only when there is no profanity usage or bulling found 
+    elif ac.check_message(msg_content):
         if ac.add_apology(author_id, channel_name):
             await message.reply("Hey <@{0}>, your apology is accepted by the bot".format(author_id))
 
-    # Step 5: Reporting a Profane Word
-    if rc.check_message(msg_content):
-        report_type, report_token = rc.parse_message(msg_content)
-        if report_type == "word":
-            if pc.add_words(channel_name, report_token):
-                await message.reply("{0} has been added as a toxic word".format(report_token))
+   
 
 if __name__ == "__main__":
     load_dotenv("bot.env")
