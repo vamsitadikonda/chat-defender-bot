3d59c942745f041b5847527543230ccf1ba3408f
svnsairam <svnsairam.sakhamuri@gmail.com>
2022-12-01 01:16:29 -0500

    Updated reporter file

--- src/checkers/reporter.py
+++ src/checkers/reporter.py
@@ -5,6 +5,8 @@ class ReportChecker(Checker):
     def check_message(self, msg:str):
         return msg.startswith('!report')
 
+    #parsing the message
     def parse_message(self, msg):
         return msg.split(" ")[1], msg.split(" ")[2]
+    
 
